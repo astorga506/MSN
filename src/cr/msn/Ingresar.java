7780fc5d06f23f722b3c63c2d8c4fec6ec55387b
Carlos Astorga <mi.emilio@outlook.com>
2014-10-18 18:58:11 -0600

    Layouts

---
+++ src/cr/msn/Ingresar.java
@@ -0,0 +1,15 @@
+package cr.msn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Ingresar extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_iniciar);
+		
+		
+	}
+}
